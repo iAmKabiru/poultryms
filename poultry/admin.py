e2a51c314c9f457e2ebb8fb7825cdd4d224a9e12
Kabiru Abdulhamid <abdulhamidkabiru@gmail.com>
2019-10-07 09:29:37 +0100

    first commit

---
+++ poultry/admin.py
@@ -0,0 +1,51 @@
+from django.contrib import admin
+from .models import BirdType, Birds, Feed, Medication, Sales, Employers, DoctorVisit, MedicalReport, Purchase, Casualty
+
+
+class BirdTypeAdmin(admin.ModelAdmin):
+	 list_display = ('bird_type', 'quantity')
+
+class BirdsAdmin(admin.ModelAdmin):
+	list_filter = ('bird_type',)
+	list_display = ('bird_type', 'description', 'quantity', 'cost_per_bird', 'date')
+
+class FeedAdmin(admin.ModelAdmin):
+	list_display = ('bird', 'description', 'date')
+
+class MedicationAdmin(admin.ModelAdmin):
+	list_filter = ('bird',)
+	list_display = ('bird', 'description', 'date')
+
+class SalesAdmin(admin.ModelAdmin):
+	list_filter = ('birds',)
+	list_display = ('birds', 'quantity', 'selling_price')
+
+class EmployersAdmin(admin.ModelAdmin):
+	list_display = ('name', 'phone', 'salary')
+
+class DoctorVisitAdmin(admin.ModelAdmin):
+	list_display = ('doctor_name', 'purpose', 'date', 'description')
+
+class MedicalReportAdmin(admin.ModelAdmin):
+	list_display = ('case', 'description', 'date')
+
+class PurchaseAdmin(admin.ModelAdmin):
+	list_filter = ('purchase_type',)
+	list_display = ('purchase_type', 'amount', 'description', 'quantity', 'date')
+
+
+class CasualtyAdmin(admin.ModelAdmin):
+	list_filter = ('casualty_type','birds')
+	list_display = ('casualty_type', 'birds', 'quantity')
+
+
+admin.site.register(BirdType, BirdTypeAdmin)
+admin.site.register(Birds, BirdsAdmin)
+admin.site.register(Feed, FeedAdmin)
+admin.site.register(Medication, MedicationAdmin)
+admin.site.register(Sales, SalesAdmin)
+admin.site.register(Employers, EmployersAdmin)
+admin.site.register(DoctorVisit, DoctorVisitAdmin)
+admin.site.register(MedicalReport, MedicalReportAdmin)
+admin.site.register(Purchase, PurchaseAdmin)
+admin.site.register(Casualty, CasualtyAdmin)
---
+++ poultry/models.py
@@ -0,0 +1,136 @@
+from django.db import models
+from django.db.models.signals import post_save, pre_save, post_delete
+from django.dispatch import receiver
+from django.db.models import F, Sum
+
+class BirdType(models.Model):
+	bird_type = models.CharField(max_length=255)
+
+	def __str__(self):
+		return self.bird_type
+
+	def quantity(self):
+		return self.birds_set.count()
+
+
+class Birds(models.Model):
+	bird_type = models.ForeignKey(BirdType, on_delete=models.CASCADE)
+	description = models.CharField(max_length = 255)
+	quantity = models.IntegerField()
+	date = models.DateTimeField(auto_now_add=True)
+	cost_per_bird = models.IntegerField(default=0, verbose_name = 'cost per bird (N)')
+
+	class Meta:
+		verbose_name_plural = 'Birds'
+
+	def __str__(self):
+		return self.description
+
+
+class Feed(models.Model):
+	bird = models.ForeignKey(Birds, on_delete=models.CASCADE)
+	description = models.CharField(max_length=255)
+	date = models.DateTimeField(auto_now_add=True)
+
+	def __str__(self):
+		return self.description
+
+
+
+class Medication(models.Model):
+	bird  = models.ForeignKey(Birds, on_delete=models.CASCADE)
+	description = models.CharField(max_length=255)
+	date = models.DateTimeField(auto_now_add = True)
+
+	def __str__(self):
+		return self.description
+
+
+
+class Sales(models.Model):
+	birds = models.ForeignKey(Birds, on_delete=models.CASCADE)
+	quantity = models.IntegerField()
+	selling_price = models.IntegerField(verbose_name = 'selling price (N)')
+
+	class Meta:
+		verbose_name_plural = 'Sales'
+
+	def __str__(self):
+		return self.birds.description
+
+
+class Employers(models.Model):
+	name = models.CharField(max_length=255)
+	phone = models.CharField(max_length=255)
+	salary = models.CharField(max_length=255, verbose_name = 'salary (N)')
+
+	class Meta:
+		verbose_name_plural = 'Employers'
+
+	def __str__(self):
+		return self.name
+
+
+class DoctorVisit(models.Model):
+	doctor_name = models.CharField(max_length=255)
+	purpose = models.CharField(max_length=255)
+	date = models.DateTimeField(auto_now_add=True)
+	description = models.TextField()
+
+	def __str__(self):
+		return self.doctor_name
+
+
+class MedicalReport(models.Model):
+	case = models.CharField(max_length=255)
+	description = models.TextField()
+	date = models.DateTimeField(auto_now_add = True)
+
+	def __str__(self):
+		return self.case
+
+class Purchase(models.Model):
+	purchase_type_choices = (
+		('feed', 'feed'),
+		('birds', 'birds'),
+		('medicine', 'medicine'),
+		('equipement', 'equipement')
+		)
+	purchase_type = models.CharField(max_length=255, choices=purchase_type_choices)
+	amount = models.IntegerField(default=0, verbose_name='Amount (N)')
+	description = models.CharField(max_length=255, blank=True, null=True)
+	quantity = models.IntegerField(default=0)
+	date = models.DateTimeField(auto_now_add=True)
+
+
+class Casualty(models.Model):
+	casualty_type_choices = (
+		('death','death'),
+		('damage', 'damage'),
+		)
+	birds = models.ForeignKey(Birds, on_delete=models.CASCADE)
+	casualty_type = models.CharField(max_length=255, choices = casualty_type_choices)
+	quantity = models.IntegerField(default=0)
+	date = models.DateTimeField(auto_now_add=True)
+
+
+	class Meta:
+		verbose_name_plural = 'Casualties'
+
+
+# signal for updating birds on casualties 
+@receiver(post_save, sender=Casualty, dispatch_uid="update_when_add")
+def update_when_add(sender, **kwargs):
+    casualty = kwargs['instance']
+    if casualty.pk:
+        Birds.objects.filter(pk=casualty.birds_id).update(quantity=F('quantity') - casualty.quantity)
+
+
+
+
+# signal for updating birds on sale
+@receiver(post_save, sender=Sales, dispatch_uid="update_when_add")
+def update_when_add(sender, **kwargs):
+    sales = kwargs['instance']
+    if sales.pk:
+        Birds.objects.filter(pk=sales.birds_id).update(quantity=F('quantity') - sales.quantity)
